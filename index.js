eaeab90dd56b17dc4937cac0270c68ca1c3a856e
vaidaabelkyte <vaidilutia@gmail.com>
2018-04-11 16:52:39 +0100

    trying to write some code for new skill - for handling basic requests and responces

---
+++ index.js
@@ -0,0 +1,208 @@
+// This is template files for developing Alexa skills
+
+
+//just a good practise to have this
+'use strict';
+
+
+//for anabling and desabling
+var winston = require('winston');
+
+//creating logger with a constructor
+var logger = new (winston.Logger)({
+    transports: [
+      new (winston.transports.Console)({ prettyPrint: true, timestamp: true, json: false, stderrLevels:['error']})
+    ]
+  });
+
+  //to hold all the intents (an empty object for a now)
+var intentHandlers = {};
+
+if(process.env.NODE_DEBUG_EN) {
+  logger.level = 'debug';
+}
+
+//usual block of code for skill development to check for application id and start of the session
+exports.handler = function (event, context) {
+    try {
+
+        logger.info('event.session.application.applicationId=' + event.session.application.applicationId);
+
+        if (APP_ID !== '' && event.session.application.applicationId !== APP_ID) {
+            context.fail('Invalid Application ID');
+         }
+      
+        if (!event.session.attributes) {
+            event.session.attributes = {};
+        }
+
+        logger.debug('Incoming request:\n', JSON.stringify(event,null,2));
+
+        if (event.session.new) {
+            onSessionStarted({requestId: event.request.requestId}, event.session);
+        }
+
+
+        if (event.request.type === 'LaunchRequest') {
+            onLaunch(event.request, event.session, new Response(context,event.session));
+        } else if (event.request.type === 'IntentRequest') {
+			
+			//creating a responce object           
+		   var response =  new Response(context,event.session);
+            if (event.request.intent.name in intentHandlers) {
+              intentHandlers[event.request.intent.name](event.request, event.session, response,getSlots(event.request));
+            } else {
+              response.speechText = 'Unknown intent';
+              response.shouldEndSession = true;
+              response.done();
+            }
+        } else if (event.request.type === 'SessionEndedRequest') {
+            onSessionEnded(event.request, event.session);
+            context.succeed();
+        }
+    } catch (e) {
+        context.fail('Exception: ' + getError(e));
+    }
+};
+
+//get slots to recognise from amazon or your own server
+function getSlots(req) {
+  var slots = {}
+  for(var key in req.intent.slots) {
+    if(req.intent.slots[key].value !== undefined) {
+      slots[key] = req.intent.slots[key].value;
+    }
+  }
+  return slots;
+}
+
+var Response = function (context,session) {
+  this.speechText = '';
+  this.shouldEndSession = true;
+  this.ssmlEn = true;
+  this._context = context;
+  this._session = session;
+
+  //after the responce is found this function will take care now if building the respone further by taking some options
+  this.done = function(options) {
+
+    if(options && options.speechText) {
+      this.speechText = options.speechText;
+    }
+
+    if(options && options.repromptText) {
+      this.repromptText = options.repromptText;
+    }
+
+    if(options && options.ssmlEn) {
+      this.ssmlEn = options.ssmlEn;
+    }
+
+    if(options && options.shouldEndSession) {
+      this.shouldEndSession = options.shouldEndSession;
+    }
+	///forming the actual response
+	//passing the instance of this object
+    this._context.succeed(buildAlexaResponse(this));
+  }
+
+  this.fail = function(msg) {
+    logger.error(msg);
+    this._context.fail(msg);
+  }
+
+};
+
+function createSpeechObject(text,ssmlEn) {
+  if(ssmlEn) {
+    return {
+      type: 'SSML',
+      ssml: '<speak>'+text+'</speak>'
+    }
+  } else {
+    return {
+      type: 'PlainText',
+      text: text
+    }
+  }
+}
+
+//by using response variable this function will built the actual response
+function buildAlexaResponse(response) {
+  var alexaResponse = {
+    version: '1.0',
+    response: {
+      outputSpeech: createSpeechObject(response.speechText,response.ssmlEn),
+      shouldEndSession: response.shouldEndSession
+    }
+  };
+
+  if(response.repromptText) {
+    alexaResponse.response.reprompt = {
+      outputSpeech: createSpeechObject(response.repromptText,response.ssmlEn)
+    };
+  }
+
+  if(response.cardTitle) {
+    alexaResponse.response.card = {
+      type: 'Simple',
+      title: response.cardTitle
+    };
+
+    if(response.imageUrl) {
+      alexaResponse.response.card.type = 'Standard';
+      alexaResponse.response.card.text = response.cardContent;
+      alexaResponse.response.card.image = {
+        smallImageUrl: response.imageUrl,
+        largeImageUrl: response.imageUrl
+      };
+    } else {
+      alexaResponse.response.card.content = response.cardContent;
+    }
+  }
+
+  if (!response.shouldEndSession && response._session && response._session.attributes) {
+    alexaResponse.sessionAttributes = response._session.attributes;
+  }
+  //printing out final response 
+  //response will be formated javascript object into json string
+  logger.debug('Final response:\n', JSON.stringify(alexaResponse,null,2));
+  return alexaResponse;
+}
+
+function getError(err) {
+  var msg='';
+  if (typeof err === 'object') {
+    if (err.message) {
+      msg = ': Message : ' + err.message;
+    }
+    if (err.stack) {
+      msg += '\nStacktrace:';
+      msg += '\n====================\n';
+      msg += err.stack;
+    }
+  } else {
+    msg = err;
+    msg += ' - This error is not object';
+  }
+  return msg;
+}
+
+
+// Skill logic 
+
+//skill application ID from amazon devloper portal will go here
+var APP_ID = '';
+
+function onSessionStarted(sessionStartedRequest, session) {
+    logger.debug('onSessionStarted requestId=' + sessionStartedRequest.requestId + ', sessionId=' + session.sessionId);
+    //session logic
+    
+}
+
+function onSessionEnded(sessionEndedRequest, session) {
+  logger.debug('onSessionEnded requestId=' + sessionEndedRequest.requestId + ', sessionId=' + session.sessionId);
+
+  
+}
+}
